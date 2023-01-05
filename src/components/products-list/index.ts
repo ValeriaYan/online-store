f3b3be5b762c0c7ae57ac0e4a3e21c0ae11b4141
aleksryab <aleksryabtsev@gmail.comm>
2023-01-05 21:03:56 +0700

    feat: add product card component

--- src/components/products-list/index.ts
+++ src/components/products-list/index.ts
@@ -2,46 +2,18 @@ import './style.scss';
 
 import { IProduct } from '../../types';
 import ComponentTemplate from '../component-template';
+import ProductCard from '../product-card';
 
 class ProductsList extends ComponentTemplate {
   constructor() {
     super('div', 'products-list');
   }
 
-  private createInfoRow(subtitle: string, text: string | number): HTMLElement {
-    const row = document.createElement('p');
-    row.className = 'product-item__info-row';
-    const subtitleBox = document.createElement('span');
-    subtitleBox.className = 'product-item__info-subtitle';
-    subtitleBox.innerText = subtitle;
-
-    row.append(subtitleBox);
-    row.append(text.toString());
-
-    return row;
-  }
-
   private createProductsList(products: IProduct[]): void {
     products.forEach((product) => {
       const productItem = document.createElement('div');
-      productItem.className = 'product-item';
-
-      const productItemTitle = document.createElement('h3');
-      productItemTitle.className = 'product-item__title';
-      productItemTitle.textContent = product.title;
-
-      const productItemInfo = document.createElement('div');
-      productItemInfo.className = 'product-item__info';
-
-      productItemInfo.append(this.createInfoRow('Category:', product.category));
-      productItemInfo.append(this.createInfoRow('Brand:', product.brand));
-      productItemInfo.append(this.createInfoRow('Price:', product.price));
-      productItemInfo.append(this.createInfoRow('Discount:', product.discountPercentage));
-      productItemInfo.append(this.createInfoRow('Rating:', product.rating));
-      productItemInfo.append(this.createInfoRow('Stock:', product.stock));
-
-      productItem.append(productItemTitle);
-      productItem.append(productItemInfo);
+      productItem.className = 'products-list__item';
+      productItem.append(new ProductCard(product).render());
       this.container.append(productItem);
     });
   }

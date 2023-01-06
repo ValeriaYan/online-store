120aa32766d1f7964876bdb59013ccd271365cfd
aleksryab <aleksryabtsev@gmail.comm>
2023-01-06 17:53:45 +0700

    feat: add no found products message

--- src/components/products-list/index.ts
+++ src/components/products-list/index.ts
@@ -9,13 +9,24 @@ class ProductsList extends ComponentTemplate {
     super('div', 'products-list');
   }
 
+  private addNoFoundMessage(): void {
+    const messageBox = document.createElement('div');
+    messageBox.className = 'products-list__no-found-message';
+    messageBox.innerText = 'No products found  ¯\\_(ツ)_/¯';
+    this.container.append(messageBox);
+  }
+
   private createProductsList(products: IProduct[]): void {
-    products.forEach((product) => {
-      const productItem = document.createElement('div');
-      productItem.className = 'products-list__item';
-      productItem.append(new ProductCard(product).render());
-      this.container.append(productItem);
-    });
+    if (products.length === 0) {
+      this.addNoFoundMessage();
+    } else {
+      products.forEach((product) => {
+        const productItem = document.createElement('div');
+        productItem.className = 'products-list__item';
+        productItem.append(new ProductCard(product).render());
+        this.container.append(productItem);
+      });
+    }
   }
 
   public render(products: IProduct[]): HTMLElement {

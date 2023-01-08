ecec8f7c5f2c3bc259e9f90f6314fd48f6f6992e
Valeria <valeria.yanycheva@gmail.com>
2023-01-08 14:04:15 +0600

    feat: implement a block of applied promo codes

--- src/components/summary/index.ts
+++ src/components/summary/index.ts
@@ -8,12 +8,14 @@ class Summary extends ComponentTemplate {
     private _total: HTMLElement;
     private _promoCode: HTMLElement;
     private _availablePromoCode: HTMLElement;
+    private _appliedCodes: HTMLElement;
     constructor(cart: Cart) {
         super('div', 'summary');
         this._cart = cart;
         this._total = new Elem('div', 'summary__total').elem;
         this._promoCode = new Elem('div', 'summary__promo-code').elem;
         this._availablePromoCode = new Elem('div', 'summary__available-promo-code').elem;
+        this._appliedCodes = new Elem('div', 'summary__applied-codes applied-codes').elem;
     }
 
     private createHeader(): HTMLElement {
@@ -70,7 +72,38 @@ class Summary extends ComponentTemplate {
 
         this._availablePromoCode.textContent = `${promoCode.toUpperCase()} - ${this._cart.getDiscount(promoCode)}%`;
         addBtn.textContent = 'Add';
+        addBtn.addEventListener('click', this.addBtnHandler.bind(this, promoCode));
         this._availablePromoCode.append(addBtn);
+    }
+
+    private createAppliedCodes() {
+        this._appliedCodes.innerHTML = '';
+        if(this._cart.thereIsPromoCode()) {
+            const header = new Elem('div', 'applied-codes__header').elem;
+            header.textContent = 'Applied Codes';
+            this._appliedCodes.append(header);
+    
+            this._cart.appliedPromoCodes.forEach((item) => {
+                const promoCode = new Elem('div', 'applied-codes__code').elem;
+                promoCode.textContent = `${item} - ${this._cart.getDiscount(item)}%`;
+                const dropBtn = <HTMLButtonElement>new Elem('button', 'applied-codes__drop-button').elem;
+                dropBtn.textContent = 'Drop';
+                dropBtn.addEventListener('click', this.dropBtnHandler.bind(this, item));
+                promoCode.append(dropBtn);
+    
+                this._appliedCodes.append(promoCode);
+            })
+        } else {
+            this._appliedCodes.remove();
+        }
+    }
+
+    private addBtnHandler(promoCode: string) {
+        this._cart.addPromoCode(promoCode);
+        this.createTotal();
+        this._promoCode.before(this._total);
+        this.createAppliedCodes();
+        this._promoCode.before(this._appliedCodes);
     }
 
     private inputPromoCodeHandler(input: HTMLInputElement) {
@@ -85,12 +118,21 @@ class Summary extends ComponentTemplate {
         }
     }
 
+    private dropBtnHandler(promoCode: string) {
+        this._cart.deletePromoCode(promoCode);
+        this.createTotal();
+        this._appliedCodes.before(this._total);
+        this.createAppliedCodes();
+    }
+
     public render(): HTMLElement {
         this.container.innerHTML = '';
         this.createTotal();
         this.createInputPromoCode();
         this.container.append(this.createHeader());
         this.container.append(this._total);
+        this.container.append(this._appliedCodes);
+        this.createAppliedCodes();
         this.container.append(this._promoCode);
         return this.container;
     }

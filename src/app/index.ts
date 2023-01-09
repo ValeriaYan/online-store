ca42bd89237202ca4ea8b4cd0c4c0d7888148f48
Valeria <valeria.yanycheva@gmail.com>
2023-01-09 22:56:11 +0600

    feat: add the total amount and the total number of products in the cart to the header

--- src/app/index.ts
+++ src/app/index.ts
@@ -25,7 +25,7 @@ class App {
     this.content = document.createElement('main');
     this.header = new Header();
     this.dataService = new DataService();
-    this.cart = new Cart().download();
+    this.cart = new Cart(this.header).download();
   }
 
   private renderPage(): void {
